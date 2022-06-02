b6c0b9d99f25184212163538c7a3cd7adfb03c2a
HP <HP@DESKTOP-V2AFGF6>
2022-06-02 23:01:01 +0530

    Add project files.

---
+++ GenerateQRCode/GenerateQRCode.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="GenerateQRCode.aspx.cs" Inherits="GenerateQRCode.GenerateQRCode" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+   <title></title>
+    <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="container">
+            <h2>Generate QR Code Using C#</h2>
+            <div class="row">
+                <div class="col-4">
+                    <div class="form-group">
+                        <label>Enter a value to generate barcode:</label>
+                        <div class="input-group">
+                            <asp:TextBox ID="txtGenerateQRCode" runat="server" CssClass="form-control"></asp:TextBox>
+                            <div class="input-group-append">
+                                <asp:Button ID="BtnGenerateQRCode" runat="server" Text="Generate QR Code" CssClass="btn btn-primary" OnClick="BtnGenerateQRCode_Click" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <asp:Image ID="ImageGeneratedBarcode" runat="server" CssClass="img-thumbnail" Visible="false"/>
+        </div>
+    </form>
+</body>
+
+</html>
+
---
+++ GenerateQRCode/GenerateQRCode.aspx.cs
@@ -0,0 +1,45 @@
+﻿using IronBarCode;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GenerateQRCode
+{
+    public partial class GenerateQRCode : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ImageGeneratedBarcode.Visible = false;
+        }
+
+        protected void BtnGenerateQRCode_Click(object sender, EventArgs e)
+        {
+            string generatebarcode = txtGenerateQRCode.Text;
+            GeneratedBarcode barcode = QRCodeWriter.CreateQrCode(generatebarcode, 300);
+
+            // Styling a QR code and adding annotation text
+
+            //barcode.AddBarcodeValueTextAboveBarcode();
+            barcode.AddBarcodeValueTextBelowBarcode();
+            barcode.SetMargins(10);
+            barcode.ChangeBarCodeColor(Color.BlueViolet);
+
+            var folder = Server.MapPath("/App_Data/GeneratedQRcode");
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+            string filePath = Server.MapPath("GeneratedQRcode/barcode.png");
+            barcode.SaveAsPng(filePath);
+            ImageGeneratedBarcode.ImageUrl = "~/GeneratedQRcode/" + Path.GetFileName(filePath);
+            ImageGeneratedBarcode.Visible = true;
+
+        }
+
+    }
+}
